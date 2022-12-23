9071b25eef8e0d0faf6c53f1ebe20453ec96641f
Andelcastillo <andresdelcastillo14@hotmail.com>
2022-12-23 13:02:52 +0100

    Se agrega la practica

---
+++ Practica2OpenBootcamp.js
@@ -0,0 +1,12 @@
+const lista={
+    nombre:"Andrés",
+    edad:27,
+    desarrollador:true,
+    fechaNacimiento:10/06/1995,
+    libro:{
+        nombre:"Todos tienen razón",
+        autor:"Paolo Sorrentino",
+        fecha:01/10/2011,
+        url:"https://www.casadellibro.com/libro-todos-tienen-razon"
+    } 
+}
